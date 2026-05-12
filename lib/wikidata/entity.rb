80de2befbd2c224693ed2f3e8eb9d2c09cb26d13
David Shorthouse <davidpshorthouse@gmail.com>
2026-05-11 21:14:22 -0400

    Return empty string in title method when no labels
    Fix title method to return an empty string if no labels or sitelinks are found.

--- lib/wikidata/entity.rb
+++ lib/wikidata/entity.rb
@@ -17,8 +17,7 @@ module Wikidata
       return labels['mul'].value if labels && labels.key?('mul')
       return labels['en'].value if labels && labels.key?('en')
       return sitelinks['en'].value if sitelinks && sitelinks.key?('en')
-
-      title
+      ""
     end
 
     def url
